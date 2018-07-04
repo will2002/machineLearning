db19e7d589169f0d710eba75e7651b4353b43bb4
WILL <william.jackson1.createthenext.com>
2018-07-04 16:23:48 -0400

    finished group sorter

--- new_project/groupGenerator.py
+++ new_project/groupGenerator.py
@@ -7,10 +7,12 @@ def main():
     people = []
     for i in range(len(sys.argv) - 2):
         people.append(str(sys.argv[i + 2]))
-    for i in range(math.floor(len(people) / groupSize)):
+    for i in range(int(math.floor(len(people) / groupSize))):
         print("Group " + str(i + 1) + ":")
-        print(people.pop(random.randrange(len(people)))
-        print(people.pop(random.randrange(len(people)))
+        print(people.pop(random.randrange(len(people))))
+        print(people.pop(random.randrange(len(people))))
+    for i in range(len(people)):
+        print(people.pop(0))
 
 
 
